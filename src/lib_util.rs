ecbe6d31b7f215b15c99d719834dd38fb127d0f8
hatsune-miku <l2a1knla@gmail.com>
2023-06-20 00:52:21 -0230

    Removed PointerWrapper

--- src/lib_util.rs
+++ src/lib_util.rs
@@ -1,31 +1,5 @@
 use std::os::raw::c_char;
 
-pub struct PointerWrapper<T> {
-    ptr: *mut T,
-}
-
-unsafe impl<T> Send for PointerWrapper<T> {}
-
-unsafe impl<T> Sync for PointerWrapper<T> {}
-
-impl<T> Clone for PointerWrapper<T> {
-    fn clone(&self) -> Self {
-        PointerWrapper { ptr: self.ptr }
-    }
-}
-
-impl<T> PointerWrapper<T> {
-    pub fn new(ptr: *mut T) -> Self {
-        PointerWrapper { ptr }
-    }
-
-    pub fn get(&self) -> *mut T {
-        self.ptr
-    }
-}
-
-// ---
-
 pub fn string_from_lengthen_ptr(ptr: *const c_char, len: u32) -> String {
     let slice = unsafe { std::slice::from_raw_parts(ptr as *const u8, len as usize) };
     String::from_utf8_lossy(slice).to_string()
